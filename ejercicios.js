97533da1b5b12be0e98091aae0bf15d9e1a4df17
pierifrancia  <pieri.francia@gmail.com>
2019-10-22 01:13:56 -0300

    ejercicios 27 a 32

---
+++ ejercicios.js
@@ -0,0 +1,61 @@
+let numeroUno = 30;
+
+let numeroDos = 20;
+
+console.log ("Ejercicio 27");
+
+console.log ("Sumo " + numeroUno + " + " + numeroDos + " y el resultado es " + (numeroUno + numeroDos));
+
+console.log ("Resto " + numeroUno + " - " + numeroDos + " y el resultado es " + (numeroUno - numeroDos));
+
+console.log ("Multiplico " + numeroUno + " x " + numeroDos + " y el resultado es " + (numeroUno * numeroDos));
+
+console.log ("Divido " + numeroUno + " / " + numeroDos + " y el resultado es " + (numeroUno / numeroDos));
+
+console.log ("Divido " + numeroUno + " / " + numeroDos + " y el resto es " + (numeroUno % numeroDos));
+
+
+console.log ("Ejercicio 28");
+
+let numero = 9;
+
+console.log (numero + " x 1 = " + (numero * 1));
+console.log (numero + " x 2 = " + (numero * 2));
+console.log (numero + " x 3 = " + (numero * 3));
+console.log (numero + " x 4 = " + (numero * 4));
+console.log (numero + " x 5 = " + (numero * 5));
+console.log (numero + " x 6 = " + (numero * 6));
+console.log (numero + " x 7 = " + (numero * 7));
+console.log (numero + " x 8 = " + (numero * 8));
+console.log (numero + " x 9 = " + (numero * 9));
+console.log (numero + " x 10 = " + (numero * 10));
+
+console.log ("Ejercicio 29");
+
+let lado = 10;
+
+console.log ("perimetro del cuadrado = " + (lado * 4));
+
+console.log ("Ejercicio 30");
+
+lado = 5;
+
+console.log ("area del cuadrado = " + (lado ** 2));
+
+console.log ("Ejercicio 31");
+
+let ladoUno = 10;
+
+let ladoDos = 20;
+
+let ladoTres = 5;
+
+console.log ("Perimetro del triangulo = " + (ladoUno + ladoDos + ladoTres));
+
+console.log ("Ejercicio 32");
+
+let altura = 10;
+
+let base = 4;
+
+console.log ("Perimetro = " + (base * 2 + altura *2) + ". Area = " + (base * altura));
